a3d27ba8099236b5741d65303b33a1910fda7772
MarkoPLK <markopylypyuk@gmail.com>
2024-03-23 22:14:15 +0100

    theme to tokyovim

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,9 +3,9 @@ return {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
-    init = function()
-      vim.cmd.colorscheme 'catppuccin-mocha'
-    end,
+    -- init = function()
+    --   vim.cmd.colorscheme 'catppuccin-mocha'
+    -- end,
     config = function()
       require('catppuccin').setup {
         integrations = {
